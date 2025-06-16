2c3a5ecdc565dbc36a29d26ae09392b4df6e1e2f
IAmHraesvelgr <alexis.kahne@free.fr>
2025-06-16 22:55:48 +0200

    config update

--- lua/hraesvelgr/settings.lua
+++ lua/hraesvelgr/settings.lua
@@ -56,6 +56,21 @@ opt.smoothscroll = true
 
 vim.diagnostic.config({
 	virtual_text = false,
+	underline = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "",
+		},
+		linehl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+		},
+		numhl = {
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
 })
 
 opt.mouse = ""
